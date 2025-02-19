6ebd9c83413ce27ec5198a9c112cecc1edfad7ed
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-02-19 14:11:48 +0100

    add unit tests for login component

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,10 +11,18 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {SessionInformation} from "../../../../interfaces/sessionInformation.interface";
+import {Observable} from "rxjs";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  let authService: AuthService;
+  let router: Router;
+  let sessionService: SessionService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,8 +37,12 @@ describe('LoginComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule]
-    })
-      .compileComponents();
+    }).compileComponents();
+
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -39,4 +51,41 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('submit', () => {
+    const sessionInformation: SessionInformation = {
+      token: 'token',
+      type: 'type',
+      id: 1,
+      username: 'karla@mail.com',
+      firstName: 'Karla',
+      lastName: 'Pérez',
+      admin: true,
+    }
+
+    it('should log the user in', () => {
+      const loginObservable: Observable<SessionInformation> = new Observable(observer => {observer.next(sessionInformation)});
+
+      jest.spyOn(authService, 'login').mockReturnValue(loginObservable);
+      jest.spyOn(sessionService, 'logIn').mockImplementation();
+      jest.spyOn(router, 'navigate').mockImplementation();
+
+      component.submit();
+
+      expect(sessionService.logIn).toHaveBeenCalledWith(sessionInformation);
+      expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
+    });
+
+    it('should show error', () => {
+      const loginObservable: Observable<SessionInformation> = new Observable(observer => {observer.error(true)});
+
+      jest.spyOn(authService, 'login').mockReturnValue(loginObservable);
+      jest.spyOn(sessionService, 'logIn').mockImplementation();
+      jest.spyOn(router, 'navigate').mockImplementation();
+
+      component.submit();
+
+      expect(component.onError).toBe(true);
+    });
+  })
 });
