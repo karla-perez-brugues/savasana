9676e537c7af9d926d619e6dd5609b64baaeaf49
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-02-25 11:01:14 +0100

    update test for auth controller

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -1,23 +1,19 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
 import com.openclassrooms.starterjwt.repository.UserRepository;
-import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -27,60 +23,23 @@ public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    AuthenticationManager authenticationManager;
-
-    @Mock
-    JwtUtils jwtUtils;
-
-    @Mock
-    PasswordEncoder passwordEncoder;
-
-    @Mock
+    @MockBean
     UserRepository userRepository;
 
-    AuthController authController;
-
-    @BeforeEach
-    public void setUp() {
-        authController = new AuthController(authenticationManager, passwordEncoder, jwtUtils, userRepository);
-    }
-
     @Test
-    public void authenticateUser_success() throws Exception {
-        String email = "karla@mail.com";
-        String password = "password";
-
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail(email);
-        loginRequest.setPassword(password);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(loginRequest);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(json))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.token").exists())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value(email));
-    }
-
-    // @Test
     public void registerUser_success() throws Exception {
-        String email = "karla3@mail.com"; // FIXME: le test passe une seule fois quand l'adresse mail est nouvelle
-        String password = "password";
-        String firstName = "Karla";
-        String lastName = "Pérez";
+        String email = "karla@mail.com";
 
         SignupRequest signupRequest = new SignupRequest();
         signupRequest.setEmail(email);
-        signupRequest.setFirstName(firstName);
-        signupRequest.setLastName(lastName);
-        signupRequest.setPassword(password);
+        signupRequest.setFirstName("Karla");
+        signupRequest.setLastName("Pérez");
+        signupRequest.setPassword("password");
 
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(signupRequest);
+
+        when(userRepository.existsByEmail(email)).thenReturn(false);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -92,18 +51,17 @@ public class AuthControllerTest {
     @Test
     public void registerUser_badRequest() throws Exception {
         String email = "yoga@studio.com";
-        String password = "test!1234";
-        String firstName = "Yoga";
-        String lastName = "Studio";
 
         SignupRequest signupRequest = new SignupRequest();
         signupRequest.setEmail(email);
-        signupRequest.setFirstName(firstName);
-        signupRequest.setLastName(lastName);
-        signupRequest.setPassword(password);
+        signupRequest.setFirstName("Yoga");
+        signupRequest.setLastName("Studio");
+        signupRequest.setPassword("test!1234");
 
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(signupRequest);
+
+        when(userRepository.existsByEmail(email)).thenReturn(true);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
