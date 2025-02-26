4dfbe15eef126292ff45eff05bb45167f8988e56
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-02-26 18:19:45 +0100

    add integration tests

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -12,11 +12,13 @@ import { RegisterComponent } from './register.component';
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs";
+import {RegisterRequest} from "../../interfaces/registerRequest.interface";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  let httpClient: HttpClient;
   let authService: AuthService;
   let router: Router;
 
@@ -34,7 +36,8 @@ describe('RegisterComponent', () => {
       ]
     }).compileComponents();
 
-    authService = TestBed.inject(AuthService);
+    httpClient = TestBed.inject(HttpClient);
+    authService = new AuthService(httpClient);
     router = TestBed.inject(Router);
 
     fixture = TestBed.createComponent(RegisterComponent);
@@ -47,25 +50,40 @@ describe('RegisterComponent', () => {
   });
 
   describe('submit', () => {
+    const registerRequest: RegisterRequest = {
+      email: 'karla@mail.com',
+      firstName: 'Karla',
+      lastName: 'Pérez',
+      password: 'password',
+    };
+
     it('should register the user', () => {
+      component.form.setValue(registerRequest);
+
       const registerObservable: Observable<void> = new Observable(observer => {observer.next()});
 
       jest.spyOn(authService, 'register').mockReturnValue(registerObservable);
+      jest.spyOn(httpClient, 'post').mockReturnValue(registerObservable);
       jest.spyOn(router, 'navigate').mockImplementation();
 
       component.submit();
 
+      expect(httpClient.post).toHaveBeenCalledWith('api/auth/register', registerRequest);
       expect(router.navigate).toHaveBeenCalledWith(['/login']);
     });
 
     it('should not register the user', () => {
+      component.form.setValue(registerRequest);
+
       const registerObservable: Observable<void> = new Observable(observer => {observer.error(true)});
 
       jest.spyOn(authService, 'register').mockReturnValue(registerObservable);
+      jest.spyOn(httpClient, 'post').mockReturnValue(registerObservable);
       jest.spyOn(router, 'navigate').mockImplementation();
 
       component.submit();
 
+      expect(httpClient.post).toHaveBeenCalledWith('api/auth/register', registerRequest);
       expect(component.onError).toEqual(true);
     })
   })
